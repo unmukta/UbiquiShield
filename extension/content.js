e5c1a3ea1164bc7f471bb2fe6b2efc4c1bd03ce3
unmukta <unmuktad@gmail.com>
2026-05-30 08:55:38 +0530

    improved fingerprint protection

--- extension/content.js
+++ extension/content.js
@@ -401,41 +401,144 @@ function enableFingerprintProtection() {
 
   try {
 
-    Object.defineProperty(
-      navigator,
-      "hardwareConcurrency",
-      {
-        get: () => 4
+  // CPU threads
+  Object.defineProperty(
+    navigator,
+    "hardwareConcurrency",
+    {
+      get: () => 8
+    }
+  )
+
+  // RAM
+  Object.defineProperty(
+    navigator,
+    "deviceMemory",
+    {
+      get: () => 8
+    }
+  )
+
+  // Platform
+  Object.defineProperty(
+    navigator,
+    "platform",
+    {
+      get: () => "Win32"
+    }
+  )
+
+  // Canvas protection
+  const originalToDataURL =
+    HTMLCanvasElement.prototype.toDataURL
+
+  HTMLCanvasElement.prototype.toDataURL =
+    function (...args) {
+
+      const ctx =
+        this.getContext("2d")
+
+      if (ctx) {
+
+        ctx.fillStyle =
+          "rgba(1,1,1,0.01)"
+
+        ctx.fillRect(
+          0,
+          0,
+          1,
+          1
+        )
+
       }
-    )
 
-    Object.defineProperty(
-      navigator,
-      "deviceMemory",
-      {
-        get: () => 8
-      }
-    )
+      return originalToDataURL.apply(
+        this,
+        args
+      )
 
-    Object.defineProperty(
-      navigator,
-      "platform",
-      {
-        get: () => "Win32"
-      }
-    )
+    }
 
-    console.log(
-      "Fingerprint protection enabled"
-    )
+// =====================
+// WEBGL PROTECTION
+// =====================
 
-  } catch {
+const originalGetExtension =
+  WebGLRenderingContext.prototype.getExtension
 
-    console.log(
-      "Fingerprint spoof failed"
+WebGLRenderingContext.prototype.getExtension =
+  function(name) {
+
+    if (
+      name === "WEBGL_debug_renderer_info"
+    ) {
+      return null
+    }
+
+    return originalGetExtension.call(
+      this,
+      name
     )
+  }
+
+const originalGetParameter =
+  WebGLRenderingContext.prototype.getParameter
+
+WebGLRenderingContext.prototype.getParameter =
+  function(param) {
+
+    if (
+      param === 37445 ||
+      param === 37446
+    ) {
+
+      return "Blocked"
+    }
+
+    return originalGetParameter.call(
+      this,
+      param
+    )
+  }
+
+      if (param === 37445)
+        return "Intel Inc."
+
+      if (param === 37446)
+        return "Intel Iris Graphics"
+
+      return originalGetParameter.call(
+        this,
+        param
+      )
+
+    }
+
+  // Battery API
+  if (navigator.getBattery) {
+
+    navigator.getBattery =
+      async () => ({
+        charging: true,
+        chargingTime: 0,
+        dischargingTime: Infinity,
+        level: 1
+      })
 
   }
+
+  console.log(
+    "Advanced fingerprint protection enabled"
+  )
+
+} catch (error) {
+
+  console.log(
+    "Fingerprint spoof failed",
+    error
+  )
+
+}
 
 }
 
